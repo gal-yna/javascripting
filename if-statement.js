35438b75c1c9b94feb15534a65ba1e3e6a228824
Galyna B <galyna.babyk@yahoo.com>
2021-11-04 17:58:46 +0000

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,14 @@
+let fruit = 'orange'
+if ('orange'.length > 5) {
+  console.log('The fruit name has more than five characters.')
+} else {
+  console.log('The fruit name has five characters or less.')
+}
+
+/* OR */
+const fruit = 'orange'
+if (fruit.length > 5) {
+  console.log('The fruit name has more than five characters.')
+} else {
+  console.log('The fruit name has five characters or less.')
+}
---
+++ rounding-numbers.js
@@ -0,0 +1,6 @@
+let roundUp = 1.5
+Math.round()
+let rounded = Math.round(roundUp)
+console.log(rounded)
+
+
